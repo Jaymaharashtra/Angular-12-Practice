cfcb8672203b31f3bba97a92ffeed94a2b4b4d00
prathamesh <prathameshap2000@gmail.com>
2023-11-26 15:59:50 +0530

    Contain for loop and nested for loop, Show data using help of array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'practice';
-  hero = 'Prabhas';
-  Year = 'Jan';
+  title = 'for loops';
+  user = ['Nayan', 'Rutuja', 'Adesh', 'Nitin', 'Gaurav'];
 
- 
- 
-
+  client = [
+    {name : 'Arush', service : ['Social Media Handle'], Location : 'Paris'},
+    {name : 'Ajay', service : ['SEO','Marketing','Client Handeling'], Location : 'India'},
+    {name : 'Nilesh', service : ['Accountant','Marketing','Client Handeling'], Location : 'Itely'},
+    {name : 'Ravi', service : ['Aadhar Enrol','License','MSME'], Location : 'India'},
+  ];
 }
